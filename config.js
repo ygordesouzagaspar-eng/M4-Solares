0deb86748fba7b8880f13fa4d11085bf39f00aa8
ygordesouzagaspar-eng <ygordesouzagaspar@gmail.com>
2026-09-02 21:40:15 -0300

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,4 @@ window.SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF6ZHllZG12YWxoZXlldXZxeGdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgzODgxNTgsImV4cCI6MjEwMzk2NDE1OH0.cVf1lxKeA8FLgxBczuZUfFhMI6Id1OKnqliHIBk08ls";
 
 /* Nome do app exibido na tela de login */
-window.APP_NAME = "Helios Solar";
+window.APP_NAME = "M4 Solar";
